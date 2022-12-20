33b9e22e41e2dc88b4cf3c16484fbf7c7d1ab192
nyeong <nyeong@users.noreply.github.com>
2022-12-20 08:02:32 +0000

    deploy: d1d7b44d254f32b56c1b854e7a62305da4632f4d

--- bin/add-frontmatter.sh
+++ bin/add-frontmatter.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
 
-find notes -name '*.md' -print0 | while read -d $'\0' file
-do
+# USAGE: find notes -name '*.md' | ./bin/add-frontmatter.sh
 
-  if grep -q '^---$' "$file"; then
-    if ! grep -q '^title:' "$file"; then
-      echo "$file has frontend block but no title."
-      # has frontmatter but no title
-      filename=$(basename "$file")
-      sed -i '' "1s/^---$/---\\
-title: ${filename%.md}/" "$file"
-    # else
-      # has frontmatter and title
-    fi
-  else
-    if [[ -z $(grep '[^[:space:]]' "$file") ]]; then
-      echo "$file is empty. removed."
-      rm "$file"
-    else
-      # has no frontmatter
-      echo "$file has no frontend block."
-      filename=$(basename "$file")
-      sed -i '' "1i\\
+# add title in given file.
+# given file must have frontmatter block.
+add_title_to() {
+  echo "[update] $1"
+  filename=$(basename "$1")
+
+  sed -i '' "1s/^---$/---\\
+title: ${filename%.md}/" "$1"
+}
+
+# create frontmatter block in given file.
+# given file must have no frontmatter block.
+create_frontmatter_at() {
+  echo "[updated] $1"
+  filename=$(basename "$1")
+  sed -i '' "1i\\
 ---\\
 title: ${filename%.md}\\
 tags: []\\
 ---\\
 \\
-" "$file"
+" "$1"
+}
+
+# remove given file
+# given file must be empty.
+remove_empty() {
+  echo "[removed] $1"
+  rm $1
+}
+
+while read file
+do
+  if grep -q '^---$' "$file"; then
+    if ! grep -q '^title:' "$file"; then
+      # has frontmatter but no title
+      add_title_to $file
+    # else
+      # has frontmatter and title
+    fi
+  else
+    if [[ -z $(grep '[^[:space:]]' "$file") ]]; then
+      remove_empty $file
+    else
+      create_frontmatter_at $file
     fi
   fi
 done
