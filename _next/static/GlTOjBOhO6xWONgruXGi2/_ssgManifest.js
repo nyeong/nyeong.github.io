ab7ec80829087122ad43448839c8710d9b0f3891
nyeong <nyeong@users.noreply.github.com>
2023-04-19 13:04:05 +0000

    deploy: f059ee93525930bd278255d82870465d683beafb

---
+++ _next/static/GlTOjBOhO6xWONgruXGi2/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fn\u002F[[...slug]]","\u002Ftags"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- static/chunks/pages/_app-033420bc1f13e19d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2888],{3543:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(1736)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[9774,179],function(){return _(3543),_(9325)}),_N_E=n.O()}]);
